ee6aea7b7dd6cb554db3bb83862e39577e9291bd
Ben Thomasson <ben.thomasson@gmail.com>
2012-04-19 17:54:44 -0400

    .

--- data_structures_and_algorithms_using_python/1/bag.py
+++ data_structures_and_algorithms_using_python/1/bag.py
@@ -19,5 +19,25 @@ class Bag:
         index = self._theItems.index(item)
         return self._theItems.pop( index )
 
-    def __iter__(self, item):
-        pass
+    def __iter__(self):
+        return _BagIterator(self._theItems)
+
+
+class _BagIterator:
+
+    def __init__(self, theList):
+        self._bagItems = theList
+        self._curItem = 0
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self._curItem < len(self._bagItems):
+            item = self._bagItems[self._curItem]
+            self._curItem += 1
+            return item
+        else:
+            raise StopIteration
+
+
--- data_structures_and_algorithms_using_python/1/test_bag.py
+++ data_structures_and_algorithms_using_python/1/test_bag.py
@@ -1,2 +1,31 @@
+#!/usr/bin/env python
 
+import unittest
+import bag
 
+class Test(unittest.TestCase):
+
+    def test_(self):
+        self.b = bag.Bag()
+        self.assertEquals(len(self.b),0)
+
+    def test_add(self):
+        self.test_()
+        self.b.add(10000)
+        self.assertEquals(len(self.b),1)
+        self.b.add(5)
+        self.assertEquals(len(self.b),2)
+
+    def test_iterator(self):
+        self.test_add()
+        i = iter(self.b)
+        self.assertEquals(next(i),10000)
+        self.assertEquals(next(i),5)
+
+    def test_iterator2(self):
+        self.test_add()
+        for i in self.b:
+            self.assert_(i)
+
+if __name__ == "__main__":
+    unittest.main()
